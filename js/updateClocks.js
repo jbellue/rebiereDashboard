d1c83822546720c189e0d4d05dd7a3cb5bfcfd15
Julien Bellue <j.bellue@gematpx.local>
2018-09-19 13:19:41 +0200

    split js in different files

---
+++ js/updateClocks.js
@@ -0,0 +1,22 @@
+var myApp = myApp || {};
+myApp.updateDecimalClock = (id) => {
+    const getDecimalTimeString = (d) => {
+        const secondsSinceMidnight = 3600*d.getHours() + 60*d.getMinutes() + d.getSeconds();
+        let metricSeconds = secondsSinceMidnight * 100000 / 86400;
+        const metricHours = Math.floor(metricSeconds / 10000);
+        metricSeconds -= 10000 * metricHours;
+        const metricMinutes = Math.floor(metricSeconds / 100);
+        metricSeconds = Math.floor(metricSeconds - 100 * metricMinutes);
+        return `${metricHours}:${metricMinutes<10?`0${metricMinutes}`:metricMinutes}:${metricSeconds<10?`0${metricSeconds}`:metricSeconds}`
+    }
+    const tick = () => {
+        document.getElementById(id).innerHTML = getDecimalTimeString(new Date())
+    };
+
+    tick();
+    window.setInterval(tick, 842); //roughly one decimal second
+}
+
+myApp.setRepublicanDate = (id) => {
+    document.getElementById(id).innerHTML = new FlorealDate().toFullDateString();
+}
